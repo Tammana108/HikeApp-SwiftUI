7a4e7d43852320d8d0542d9bdd5e54e31988766c
Dhananjay Chhabra <dhananjay.dj.chhabra@gmail.com>
2023-12-17 17:50:51 +0530

    Initial Commit

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by Tammana Sharma on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
